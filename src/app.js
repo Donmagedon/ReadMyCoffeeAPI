67b7f2aa33825c1d1543f426ffe1f0a7b5dbbd0b
donmagedon <donmagedon7@gmail.com>
2025-03-05 23:07:51 -0500

    add vercel file, after testing different hostings

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const questions = require("./routes/questions");
 const bodyParser = require('body-parser')
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", true);
@@ -15,6 +15,6 @@ app.use((req, res, next) => {
 app.use(bodyParser.json())
 app.use("/api/generate_coffee", generate_coffee);
 app.use("/api/questions", questions);
-app.listen(3030, () => {
+app.listen(3030,"0.0.0.0", () => {
   console.log("I'm alive!");
 });
