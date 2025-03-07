e79835632f065af5f61628cdd202cde0a5029dec
donmagedon <donmagedon7@gmail.com>
2025-03-06 19:22:01 -0500

    doing away with vercel and adding authorizations headers to deal with CORS

--- src/app.js
+++ src/app.js
@@ -5,14 +5,14 @@ const generate_coffee = require("./routes/generate_coffee");
 const questions = require("./routes/questions");
 const bodyParser = require('body-parser')
 
-// {app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST");
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-//   next();
-// });
-// }
+{app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+}
 app.use(bodyParser.json())
 app.get("/",(req,res)=>{
   res.send("Read my coffee")
